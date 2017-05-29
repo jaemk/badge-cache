562d2594302815c0ae6c6d8166030e7fd978a179
Michal Budzynski <budziq@gmail.com>
2017-05-29 17:38:17 +0200

    tweak the routes to ideally mimic shields.io behaviour for crates
    http://localhost:3000/crates/v/clap.svg?label=clap

--- src/routes.rs
+++ src/routes.rs
@@ -8,6 +8,6 @@ use handlers;
 /// Mount our urls and routers on our `Router`
 pub fn mount(router: &mut Router) {
     router.get("/", Static::new("static/index.html"), "home");
-    router.get("/crate/:cratename", handlers::krate, "crate");
+    router.get("/crates/v/:cratename", handlers::krate, "crate");
     router.get("/badge/:badgeinfo", handlers::badge, "badge");
 }
